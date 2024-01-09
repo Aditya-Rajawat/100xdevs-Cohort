655c5a06bf1db4b319691c11b90c22c6f01ef8e9
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-09 21:57:49 +0530

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 0-100 Harkirat Cohort (MERN Stack)
 
 1. Learning Express.js.
+2. Creating Hospital management tasks
--- index.js
+++ index.js
@@ -22,26 +22,24 @@ app.get("/", (req, res) => {
   const numberofKidneys = users[0].kidneys.length;
 
   // Number of healthy kidneys
-  const numberofHealthyKidneys = users.filter(
-    (user) => user.kidneys[0].healthy === true
-  );
+  const HealthyKidneys = users[0].kidneys
+  const TotalHealthyKidneys = HealthyKidneys.filter((kidney) => kidney.healthy===true);
+  console.log(TotalHealthyKidneys)
 
-  const TotalNumberofKidneys = numberofKidneys;
-  const TotalNumberofHealthyKidneys = numberofHealthyKidneys.length;
+  const TotalNumberofKidneys = numberofKidneys;  
+  const TotalNumberofHealthyKidneys = TotalHealthyKidneys.length;  
   const TotalNumberofUnHealthyKidneys = TotalNumberofKidneys - TotalNumberofHealthyKidneys
 
   res.json({TotalNumberofKidneys, TotalNumberofHealthyKidneys, TotalNumberofUnHealthyKidneys});
 });
 
-// How many john kidneys are healthy
-app.get("/", (req, res) => {});
-
 // for POST request popular input type is - body
 app.post("/", (req, res) => {
-  const isHealthy = req.body.isHealthy;
+  const isnewHealthy = req.body.isHealthy;
   users[0].kidneys.push({
-    healthy: isHealthy,
+    healthy: isnewHealthy,
   });
+  console.log(users[0].kidneys)
   res.json({
     message: "Done!",
   });
@@ -49,13 +47,14 @@ app.post("/", (req, res) => {
 
 // Replacting all unhealthy kidneys with healthy one's
 app.put("/", (req, res) => {
-  // const UpdatedKidneys =  users.forEach((hkidneys) => {
-  //     hkidneys.kidneys[0].healthy = true
-  // })
+    const UserKidneys = users[0].kidneys
 
-  for (let i = 0; i < users[0].kidneys.length; i++) {
-    users[0].kidneys[i].healthy = true;
-  }
+    UserKidneys.map((kidney) => {
+        kidney.healthy = true
+    })
+//   for (let i = 0; i < users[0].kidneys.length; i++) {
+//     users[0].kidneys[i].healthy = true;
+//   }
   res.json({
     Mission: "Successfull !",
   });
