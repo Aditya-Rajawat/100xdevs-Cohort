a53b1856477b03272b577da346937d877bed6c43
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-09 21:30:43 +0530

    first commit

--- index.js
+++ index.js
@@ -1,70 +1,66 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-
-// // function sum(n){
-// //     let ans = 0;
-// //     for (i=0; i<n; i++){
-// //       ans = ans + i;
-// //     }
-// //     return ans
-// // }
-
-// // sum()
-
-// // app.get("/", (req, res) => {
-// //     const n = req.query.n
-// //     res.status(200).send("value of n is " + n)
-// // })
-
-// const users = [{
-//     name : "max",
-//     kidneys: [{
-//         healthy: false
-//     }]
-// }];
-
-// // How many kidneys max have
-// // How many unhealthy kidneys does max have
-// // How many healthy kidneys does max have
-
-// app.get("/", (req, res) => {
-//     // How many kidneys does max have
-//     const MaxKidneys = users[0].kidneys.length
-//     // How many unhealthy kidneys does max have
-//     const unhealthyKidneys = users.filter((hkidneys) => {
-//         hkidneys.kidneys.healthy = false
-//     })
-//     console.log(unhealthyKidneys)
-//     res.json([
-//         MaxKidneys,
-//         unhealthyKidneys
-//     ])
-// })
-
-// app.listen(port, () => {
-//     console.log(`Server is running on localhost ${port}`)
-// })
-
-
-
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 const port = 3000;
 
-var users = [{
-    name : "John",
-    kidneys : [{
-        healthy : false
-    }]
-}]
+var users = [
+  {
+    name: "John",
+    kidneys: [
+      {
+        healthy: false,
+      }
+    ],
+  },
+];
 
-// how many kidneys does john have and his kidney health
+app.use(express.json());
 
+// for GET request popular input type is - query parameters
 
+// Number of kidneys john have
+app.get("/", (req, res) => {
+  const numberofKidneys = users[0].kidneys.length;
+
+  // Number of healthy kidneys
+  const numberofHealthyKidneys = users.filter(
+    (user) => user.kidneys[0].healthy === true
+  );
+
+  const TotalNumberofKidneys = numberofKidneys;
+  const TotalNumberofHealthyKidneys = numberofHealthyKidneys.length;
+  const TotalNumberofUnHealthyKidneys = TotalNumberofKidneys - TotalNumberofHealthyKidneys
+
+  res.json({TotalNumberofKidneys, TotalNumberofHealthyKidneys, TotalNumberofUnHealthyKidneys});
+});
+
+// How many john kidneys are healthy
+app.get("/", (req, res) => {});
+
+// for POST request popular input type is - body
+app.post("/", (req, res) => {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    message: "Done!",
+  });
+});
+
+// Replacting all unhealthy kidneys with healthy one's
+app.put("/", (req, res) => {
+  // const UpdatedKidneys =  users.forEach((hkidneys) => {
+  //     hkidneys.kidneys[0].healthy = true
+  // })
+
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({
+    Mission: "Successfull !",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server started on locahost ${port}`)
-})
-
-
+  console.log(`Server started on locahost ${port}`);
+});
