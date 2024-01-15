32a0c17838f3d9bc91f0d0a5525238096fb4d01c
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-15 16:01:00 +0530

    new commit

--- middlewares.js
+++ middlewares.js
@@ -4,24 +4,33 @@ const app = express();
 
 const port = 3000;
 
-app.get("/health-checkup", (req, res) => {
+function userMiddleware(req, res, next) {
+  if (username === "Aditya" && password === 12345) {
+    next();
+  } else {
+    res.json({
+      msg: "Bad inputs",
+    });
+  }
+}
+
+function kidneyMiddleware(req, res, next) {
+  if (kidneyId == 1 || kidneyId == 2) {
+    next();
+  } else {
+    res.json({
+      msg: "authentication failed!!",
+    });
+  }
+}
+
+app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req, res) => {
   const username = req.headers.username;
   const password = req.headers.password;
   const kidneyId = req.query.kidneyId;
-
-  //   Input Validation
-  if (username === "aditya" && password == "12345") {
-    res.json({
-      msg: "Validation Done!!",
-    });
-  }
-
-  //   Authentication
-  if (kidneyId == 1 || kidneyId == 2) {
-    res.json({
-      msg: "Authentication Done!!",
-    });
-  }
+  res.send({
+    msg: "your kidneys are fine",
+  });
 });
 
 app.listen(port, () => {
