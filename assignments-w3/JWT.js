180813e891b22ca8a6a9e31e085b7ecd71bde47b
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-19 13:44:38 +0530

    commit

--- assignments-w3/JWT.js
+++ assignments-w3/JWT.js
@@ -1,24 +1,25 @@
 const jwt = require("jsonwebtoken");
 const express = require("express");
-const port = 3000;
+const zod = require("zod");
 const jwtPassword = "secret";
 
 const app = express();
 
-const contents = {
-  name: "Aditya",
-  branchname: "Canara Bank",
-  accountnumber: "SBI1234",
-};
+const emailSchema = zod.string().email()
+const passwordSchema = zod.string().min(6)
 
-const token = jwt.sign(contents, jwtPassword);
-console.log(token)
+function signJwt(username, password){
+    const usernameResponse = emailSchema.safeParse(username)
+    const passwordResponse = passwordSchema.safeParse(password)
+    if(!usernameResponse.success || !passwordResponse.success){
+        console.log(null)
+    }
+    const signature = jwt.sign({username}, jwtPassword)
+    console.log(signature, "done sir")
+}
 
-const decodeverification = jwt.verify(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRpdHlhIiwiYnJhbmNobmFtZSI6IkNhbmFyYSBCYW5rIiwiYWNjb3VudG51bWJlciI6IlNCSTEyMzQiLCJpYXQiOjE3MDU2NDk2MTN9.j8nQSsK_OlXqzWCTejMtXP6ZIZcO7xJ4kcaCw91U-NE",
-  jwtPassword
-);
+signJwt("adityarajawat04@gmail.com", "1224222222")
 
-console.log(decodeverification);
-
-app.listen(port);
+app.listen(3000)
+// - Write a function that takes in a username and password and returns a JWT token with the username encoded. 
+// Should return null if the username is not a valid email or if the password is less than 6 characters. Try using the zod library here
