8194e555ce731bd0e45579494487e715ca8f70a6
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-20 21:51:28 +0530

    commit

--- dom.js
+++
@@ -1,6 +0,0 @@
-const domheading = document.getElementsByClassName("dom")
-const btn = document.getElementById("btn")
-
-const newData = document.addEventListener("click", () => {
-    domheading.innerHTML = "Heading has changed"
-})
