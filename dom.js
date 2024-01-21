99a2d8f0148cb18ca83ddc74acbddd16d41c9fc9
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-21 19:34:39 +0530

    new commit

--- dom.js
+++ dom.js
@@ -2,3 +2,11 @@ function handleClick() {
     const mainheading = document.getElementsByClassName("para")
     mainheading.innerText = "Heading changed"
 }
+
+const heading = document.getElementById("heading")
+
+const button = document.getElementById("btn")
+button.addEventListener("mouseover", () => {
+    heading.innerHTML = "New heading"
+    console.log("clicked")
+})
