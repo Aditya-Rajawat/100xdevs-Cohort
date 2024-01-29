519f52adf28c01c6a6d95b5a1317fbffb6fd9906
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-29 20:06:36 +0530

    middleware fixed

--- assignment-w4/middleware/admin.js
+++ assignment-w4/middleware/admin.js
@@ -1,5 +1,10 @@
 function adminMiddleware (req, res, next) {
     // logic
+    const username = req.headers.username;
+    const password = req.headers.password;
+    res.json({
+        message : "Admin created successfully"
+    })
 }
 
 module.exports = adminMiddleware;
