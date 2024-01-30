1d5e888db7735473592092d7ffc0d22d2d0cd5ca
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-30 19:28:31 +0530

    user side fixed

--- assignment-w4/middleware/user.js
+++ assignment-w4/middleware/user.js
@@ -1,24 +1,22 @@
 const { User } = require("../db/index");
 
-function userMiddleware(req, res, next) {
+async function userMiddleware(req, res, next) {
   // Implement user auth logic
   // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
   const username = req.headers.username;
   const password = req.headers.password;
-  User.findOne({
+  const isExist=await User.findOne({
     username: username,
     password: password,
-  }).then({
-    function(value) {
-      if (value) {
-        next();
-      } else {
-        res.status(403).json({
-          msg: "User doesn't exist",
-        });
-      }
-    },
   });
+  if(isExist){
+    next();
+  }
+  else{
+    res.status(403).json({
+        msg:"User does not exist!!"
+    })
+  }
 }
 
 module.exports = userMiddleware;
