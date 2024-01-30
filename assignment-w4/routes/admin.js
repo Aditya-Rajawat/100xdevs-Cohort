19c9be145ac7c2204a9fd77ef11301b8b06fdd06
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-30 13:38:04 +0530

    admin panel tested

--- assignment-w4/routes/admin.js
+++ assignment-w4/routes/admin.js
@@ -1,21 +1,31 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+  await Admin.create({
+    username: username,
+    password: password,
+  });
+  res.json({
+    message: "Admin created successfully",
+  });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, (req, res) => {
+  // Implement course creation logic
 });
 
-router.get('/courses', (req, res) => {
-    // Implement fetching all courses logic
-    res.json({
-        msg : "Hello"
-    })
+router.get("/courses", (req, res) => {
+  // Implement fetching all courses logic
+  res.json({
+    msg: "Hello",
+  });
 });
 
 module.exports = router;
