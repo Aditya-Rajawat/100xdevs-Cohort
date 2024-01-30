a9699001c62d7ae7b6ae7437da45c449e8556468
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-01-30 13:07:15 +0530

    commit

--- assignment-w4/middleware/user.js
+++ assignment-w4/middleware/user.js
@@ -1,5 +1,6 @@
-function UserMiddleware (req, res, next) {
-    // logic
+function userMiddleware(req, res, next) {
+    // Implement user auth logic
+    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
 }
 
-module.exports = UserMiddleware
+module.exports = userMiddleware;
