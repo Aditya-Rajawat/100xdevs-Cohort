d8304175902d4f4306747620a1972ef08406cbd2
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-02-09 19:57:29 +0530

    cmts

--- React-Todo/frontend/vite-project/src/App.jsx
+++ React-Todo/frontend/vite-project/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [mainTask, setMainTask] = useState([]);
 
   const handleClick = () => {
-    setMainTask(title , desc);
+    setMainTask(title + " " + desc);
   };
 
   // mainTask.map((t, i) => {
