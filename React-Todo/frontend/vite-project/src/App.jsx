0c41a90835cf5a68e83392b72441ec5e48ef3bb1
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-02-09 19:54:46 +0530

    commit

--- React-Todo/frontend/vite-project/src/App.jsx
+++ React-Todo/frontend/vite-project/src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
           />
         </div>
         <button
-          className="bg-green-400-700 p-4 rounded-full text-white font-semibold hover:drop-shadow-xl"
+          className="bg-green-400 p-4 rounded-full text-white font-semibold hover:drop-shadow-xl"
           onClick={handleClick}
         >
           Add Task
