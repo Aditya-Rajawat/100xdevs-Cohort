47d6f4fd62777fdf029a01546941ed1d284c86b8
Aditya-Rajawat <adityarajawat04@gmail.com>
2024-02-14 20:23:31 +0530

    cmts

--- React-Todo/frontend/vite-project/src/App.jsx
+++ React-Todo/frontend/vite-project/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <>
       <div className="flex flex-col gap-10 justify-center items-center m-6">
         <span className="text-3xl font-semibold hover:underline hover:cursor-pointer">
-          Todo App using ReactJS
+          Todo App
         </span>
 
         <div className="flex gap-5">
